774d342b844554f2e019ac23a8a45bd8908d094f
andresns <andresm.navarro@gmail.com>
2020-09-01 10:20:38 -0400

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require("postman-request");
+
+const forecast = (location, callback)=>{
+    const url = "http://api.weatherstack.com/current?access_key=5cfede3e592ebc4724a246eab2109997&query="+location;
+
+    request({
+            url: url,
+            json: true
+        },
+        (error, response) => {
+            if (error) {
+                callback('Unable to connect to weather service!', undefined);
+            } else if (response.body.error) {
+                callback('Unable to find location', undefined);
+            } else {
+                const temp = response.body.current.temperature;
+                const rain = response.body.current.precip;
+                callback(undefined, `It is currently ${temp} degrees out. There is a ${rain} chance of rain.`);
+            }
+        });
+};
+
+module.exports = forecast
